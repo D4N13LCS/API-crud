08a5014263a273d1b237cb8ff80dc14dd448868d
CardosoDaniel <145879116+D4N13LCS@users.noreply.github.com>
2024-06-06 16:16:44 -0300

    alteração do host

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ var pool = db.createPool(
     "user": "j3owxxv8ftnpckh1",
     "password": "vefacmybz4gfuapr",
     "database": "lv79hage45wfl1qr",
-    "host": "h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306",
+    "host": "h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "port": 3306,
 
 }
