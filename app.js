8b4e5918436f2c43adfb17126317b2a79e2f5fe5
CardosoDaniel <danielmazzini6@gmail.com>
2025-02-15 18:00:43 -0300

    Correção app.dockerfile

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 const express = require('express');
 const app = express(); 
-
+const cors = require('cors');
 const rotaDeProdutos = require('./routes/produtos');
 const bodyParser = require('body-parser');
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false})); 
 
-app.use((req, res, next)=>{
-    res.header('Access-Control-Allow-Origin', '*') 
-    res.header('Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    )
-
-    if(req.method === 'OPTIONS'){ 
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); 
-        return res.status(200).json({});
-    }
-
-    next();
-});
 
 app.use('/produtos', rotaDeProdutos);
 
